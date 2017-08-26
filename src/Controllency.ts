af59840e72b88ce69cf9f0a33d83e68cb1fe884a
David López Pérez <david.lopez@xepient.com>
2017-08-26 20:31:20 +0200

    Added more unit test and finished implementing basic usage

--- src/Controllency.ts
+++ src/Controllency.ts
@@ -46,7 +46,8 @@ export class Controllency extends EventEmitter {
         this.status = 'paused';
     }
     public resume(): void {
-        this.status = this.buffer.length === 0 ? 'idle' : 'processing';
+        this.status = this.currentQuantityProcessing === 0 ? 'idle' : 'processing';
+        this.proceed();
     }
     public getCurrentQuantityProcessing(): number {
         return this.currentQuantityProcessing;
@@ -67,36 +68,54 @@ export class Controllency extends EventEmitter {
         if (typeof item.fn !== 'function') {
             throw new Error('"fn" must be a function');
         }
-        let promise = Promise.resolve(item.fn.apply(item.thisObj, item.params));
         let bufferedItem: ControllencyBufferedItem = {
             bufferedDate: new Date(),
-            promise: promise,
+            promise: null,
             fn: item.fn,
             params: item.params,
             thisObj: item.thisObj,
             processing: true
         };
         this.buffer.push(bufferedItem);
-        this.status = 'processing';
-        this.currentQuantityProcessing += 1;
-        promise.then(this.onPromiseResolved.bind(this, bufferedItem), this.onPromiseRejected.bind(this, bufferedItem));
+        this.proceed();
+    }
+
+    private proceed(): void{
+        if (this.maxConcurrency === this.currentQuantityProcessing || this.status === 'paused') { 
+            return;
+        }
+        let quantityToStart = Math.min(this.maxConcurrency - this.currentQuantityProcessing, this.buffer.length - this.currentQuantityProcessing);
+        let itemsToStart = this.buffer.slice(this.currentQuantityProcessing, this.currentQuantityProcessing + quantityToStart);
+        this.currentQuantityProcessing += quantityToStart;
+        this.status = this.currentQuantityProcessing === 0 ? 'idle' : 'processing';
+        itemsToStart.forEach(this.startItem.bind(this));
+    }
+
+    private startItem(bufferedItem: ControllencyBufferedItem): void {
+        if (typeof bufferedItem !== 'object' || bufferedItem.promise !== null) {
+            return; // if by some reason startItem is called with an already initialized bufferedItem, just ignore it. This should never happen.
+        }
+        bufferedItem.promise = Promise.resolve(bufferedItem.fn.apply(bufferedItem.thisObj, bufferedItem.params));
+        bufferedItem.promise.then(this.onPromiseResolved.bind(this, bufferedItem), this.onPromiseRejected.bind(this, bufferedItem));
     }
 
     private onPromiseResolved(bufferedItem: ControllencyBufferedItem, result: any): void {
         this.currentQuantityProcessing -= 1;
         this.buffer.splice(this.buffer.indexOf(bufferedItem), 1);
-        if (this.buffer.length === 0) {
+        if (this.currentQuantityProcessing === 0) {
             this.status = 'idle';
         }
+        this.proceed();
         this.emit('resolved', result, bufferedItem);
     }
 
     private onPromiseRejected(bufferedItem: ControllencyBufferedItem, reason: any): void {
         this.currentQuantityProcessing -= 1;
         this.buffer.splice(this.buffer.indexOf(bufferedItem), 1);
-        if (this.buffer.length === 0) {
+        if (this.currentQuantityProcessing === 0) {
             this.status = 'idle';
         }
+        this.proceed();
         this.emit('rejected', reason, bufferedItem);
     }
 }
--- test/Controllency.spec.ts
+++ test/Controllency.spec.ts
@@ -5,27 +5,27 @@ import { Controllency } from "../src/Controllency";
 import { ControllencyStatus } from "../src/ControllencyStatus";
 use(chaiAsPromised);
 
-it('should be initialized with maxConcurrency = 1', () => { 
+it('should be initialized with maxConcurrency = 1', () => {
     let controllency = new Controllency();
     expect(controllency.getMaxConcurrency()).to.be.eql(1);
 });
 
-it('should be initialized with currentQuantityProcessing = 0', () => { 
+it('should be initialized with currentQuantityProcessing = 0', () => {
     let controllency = new Controllency();
     expect(controllency.getCurrentQuantityProcessing()).to.be.eql(0);
 });
 
-it('should be initialized with an empty buffer', () => { 
+it('should be initialized with an empty buffer', () => {
     let controllency = new Controllency();
     expect(controllency.getBufferSize()).to.be.eql(0);
 });
 
-it('should be initialized with "idle" status', () => { 
+it('should be initialized with "idle" status', () => {
     let controllency = new Controllency();
     expect(controllency.getStatus()).to.be.eql('idle' as ControllencyStatus);
 });
 
-it('should have status "idle", then "processing", and finally "idle" after an item is pushed and resolved', (done) => { 
+it('should have status "idle", then "processing", and finally "idle" after an item is pushed and resolved', (done) => {
     let controllency = new Controllency();
     let fn = function fn(): Promise<any> {
         return new Promise((resolve, reject) => {
@@ -37,14 +37,14 @@ it('should have status "idle", then "processing", and finally "idle" after an it
     controllency.push(fn);
     expect(controllency.getStatus()).to.be.eql('processing' as ControllencyStatus);
     expect(controllency.getCurrentQuantityProcessing()).to.be.eql(1);
-    controllency.on('resolved', () => { 
+    controllency.on('resolved', () => {
         expect(controllency.getStatus()).to.be.eql('idle' as ControllencyStatus);
         expect(controllency.getCurrentQuantityProcessing()).to.be.eql(0);
         done();
-    })
+    });
 });
 
-it('should have status "idle", then "processing", and finally "idle" after an item is pushed and rejected', (done) => { 
+it('should have status "idle", then "processing", and finally "idle" after an item is pushed and rejected', (done) => {
     let controllency = new Controllency();
     let fn = function fn(): Promise<any> {
         return new Promise((resolve, reject) => {
@@ -56,9 +56,143 @@ it('should have status "idle", then "processing", and finally "idle" after an it
     controllency.push(fn);
     expect(controllency.getStatus()).to.be.eql('processing' as ControllencyStatus);
     expect(controllency.getCurrentQuantityProcessing()).to.be.eql(1);
-    controllency.on('rejected', () => { 
+    controllency.on('rejected', () => {
         expect(controllency.getStatus()).to.be.eql('idle' as ControllencyStatus);
         expect(controllency.getCurrentQuantityProcessing()).to.be.eql(0);
         done();
-    })
+    });
+});
+
+it('should queue items without start processing them if status is paused', () => {
+    let controllency = new Controllency();
+    let fnCalledCount = 0;
+    let fn = function fn(): Promise<any> {
+        fnCalledCount += 1;
+        return new Promise((resolve, reject) => {
+            setImmediate(reject);
+        });
+    };
+    expect(controllency.getStatus()).to.be.eql('idle' as ControllencyStatus);
+    expect(controllency.getBufferSize()).to.be.eql(0);
+    controllency.pause();
+    expect(controllency.getStatus()).to.be.eql('paused' as ControllencyStatus);
+    controllency.push(fn);
+    expect(controllency.getStatus()).to.be.eql('paused' as ControllencyStatus);
+    expect(controllency.getCurrentQuantityProcessing()).to.be.eql(0);
+    expect(fnCalledCount).to.be.eql(0);
+    expect(controllency.getBufferSize()).to.be.eql(1);
+    controllency.push(fn);
+    controllency.push(fn);
+    controllency.push(fn);
+    expect(controllency.getStatus()).to.be.eql('paused' as ControllencyStatus);
+    expect(controllency.getCurrentQuantityProcessing()).to.be.eql(0);
+    expect(fnCalledCount).to.be.eql(0);
+    expect(controllency.getBufferSize()).to.be.eql(4);
+});
+
+it('should start processing queue items after resume', () => {
+    let controllency = new Controllency();
+    let fnCalledCount = 0;
+    let fn = function fn(): Promise<any> {
+        fnCalledCount += 1;
+        return new Promise((resolve, reject) => {
+            setImmediate(reject);
+        });
+    };
+    controllency.pause();
+    controllency.push(fn);
+    expect(fnCalledCount).to.be.eql(0);
+    controllency.resume();
+    expect(fnCalledCount).to.be.eql(1);
+    expect(controllency.getCurrentQuantityProcessing()).to.be.eql(1);
+    expect(controllency.getBufferSize()).to.be.eql(1);
+});
+
+it('should not rebase maxConcurrency if a batch of items are pushed at the same time', (done) => {
+    let controllency = new Controllency({ maxConcurrency: 3 });
+    let fnCalledCount = 0;
+    let fn = function fn(): Promise<any> {
+        fnCalledCount += 1;
+        return new Promise((resolve, reject) => {
+            setImmediate(resolve);
+        });
+    };
+    controllency.push(fn);
+    controllency.push(fn);
+    controllency.push(fn);
+    controllency.push(fn);
+    controllency.push(fn);
+    controllency.push(fn);
+    expect(fnCalledCount).to.be.eql(3);
+    expect(controllency.getCurrentQuantityProcessing()).to.be.eql(3);
+    expect(controllency.getBufferSize()).to.be.eql(6);
+    controllency.on('resolved', () => {
+        expect(controllency.getCurrentQuantityProcessing()).to.be.lessThan(4);
+        if (fnCalledCount === 6 && controllency.getBufferSize() === 0 && controllency.getCurrentQuantityProcessing() === 0) {
+            expect(controllency.getStatus()).to.be.eql('idle' as ControllencyStatus);
+            done();
+        }
+    });
+});
+
+it('should not rebase maxConcurrency if various batches of items are being pushed while they are being resolved', (done) => {
+    let controllency = new Controllency({ maxConcurrency: 3 });
+    let fnCalledCount = 0;
+    let fnPromisesResolved = 0;
+    let promiseResolver: (() => void)[] = [];
+    let fn = function fn(): Promise<any> {
+        fnCalledCount += 1;
+        return new Promise((resolve, reject) => {
+            promiseResolver.push(resolve);
+        });
+    };
+    controllency.push(fn);
+    controllency.push(fn);
+    expect(fnCalledCount).to.be.eql(2);
+    expect(controllency.getCurrentQuantityProcessing()).to.be.eql(2);
+    expect(controllency.getBufferSize()).to.be.eql(2);
+    controllency.push(fn);
+    controllency.push(fn);
+    expect(fnCalledCount).to.be.eql(3);
+    expect(controllency.getCurrentQuantityProcessing()).to.be.eql(3);
+    expect(controllency.getBufferSize()).to.be.eql(4);
+    controllency.push(fn);
+    controllency.push(fn);
+    expect(fnCalledCount).to.be.eql(3);
+    expect(controllency.getCurrentQuantityProcessing()).to.be.eql(3);
+    expect(controllency.getBufferSize()).to.be.eql(6);
+    controllency.on('resolved', () => {
+        expect(controllency.getCurrentQuantityProcessing()).to.be.lessThan(4);
+        expect(fnCalledCount - fnPromisesResolved).to.be.lessThan(4);
+        if (fnCalledCount === 20 && controllency.getBufferSize() === 0 && controllency.getCurrentQuantityProcessing() === 0) {
+            expect(controllency.getStatus()).to.be.eql('idle' as ControllencyStatus);
+            done();
+        }
+        else {
+            if (fnPromisesResolved === 4) { 
+                controllency.push(fn);
+                controllency.push(fn);
+                controllency.push(fn);
+                controllency.push(fn);
+            }
+            if (fnPromisesResolved === 8) { 
+                controllency.push(fn);
+                controllency.push(fn);
+                controllency.push(fn);
+                controllency.push(fn);
+                controllency.push(fn);
+                controllency.push(fn);
+                controllency.push(fn);
+                controllency.push(fn);
+                controllency.push(fn);
+            }
+            if (fnPromisesResolved === 19) { 
+                controllency.push(fn);
+            }
+            promiseResolver[fnPromisesResolved]();
+            fnPromisesResolved += 1;
+        }
+    });
+    promiseResolver[fnPromisesResolved]();
+    fnPromisesResolved += 1;
 });
