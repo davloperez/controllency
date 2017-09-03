d5325eb37f8a19864c2ca4f995e61293ec5cad06
David López Pérez <david.lopez@xepient.com>
2017-09-03 23:29:25 +0200

    Added configuration for code coverage measuring using istanbul. Fixed some minor code style issues with tslint.

--- test/Controllency.spec.ts
+++ test/Controllency.spec.ts
@@ -6,28 +6,28 @@ import { ControllencyStatus } from "../src/ControllencyStatus";
 use(chaiAsPromised);
 
 it('should be initialized with maxConcurrency = 1', () => {
-    let controllency = new Controllency();
+    const controllency = new Controllency();
     expect(controllency.getMaxConcurrency()).to.be.eql(1);
 });
 
 it('should be initialized with currentQuantityProcessing = 0', () => {
-    let controllency = new Controllency();
+    const controllency = new Controllency();
     expect(controllency.getCurrentQuantityProcessing()).to.be.eql(0);
 });
 
 it('should be initialized with an empty buffer', () => {
-    let controllency = new Controllency();
+    const controllency = new Controllency();
     expect(controllency.getBufferSize()).to.be.eql(0);
 });
 
 it('should be initialized with "idle" status', () => {
-    let controllency = new Controllency();
+    const controllency = new Controllency();
     expect(controllency.getStatus()).to.be.eql('idle' as ControllencyStatus);
 });
 
 it('should have status "idle", then "processing", and finally "idle" after an item is pushed and resolved', (done) => {
-    let controllency = new Controllency();
-    let fn = function fn(): Promise<any> {
+    const controllency = new Controllency();
+    const fn = (): Promise<any> => {
         return new Promise((resolve, reject) => {
             setImmediate(resolve);
         });
@@ -45,8 +45,8 @@ it('should have status "idle", then "processing", and finally "idle" after an it
 });
 
 it('should have status "idle", then "processing", and finally "idle" after an item is pushed and rejected', (done) => {
-    let controllency = new Controllency();
-    let fn = function fn(): Promise<any> {
+    const controllency = new Controllency();
+    const fn = (): Promise<any> => {
         return new Promise((resolve, reject) => {
             setImmediate(reject);
         });
@@ -64,9 +64,9 @@ it('should have status "idle", then "processing", and finally "idle" after an it
 });
 
 it('should queue items without start processing them if status is paused', () => {
-    let controllency = new Controllency();
+    const controllency = new Controllency();
     let fnCalledCount = 0;
-    let fn = function fn(): Promise<any> {
+    const fn = (): Promise<any> => {
         fnCalledCount += 1;
         return new Promise((resolve, reject) => {
             setImmediate(reject);
@@ -91,9 +91,9 @@ it('should queue items without start processing them if status is paused', () =>
 });
 
 it('should start processing queue items after resume', () => {
-    let controllency = new Controllency();
+    const controllency = new Controllency();
     let fnCalledCount = 0;
-    let fn = function fn(): Promise<any> {
+    const fn = (): Promise<any> => {
         fnCalledCount += 1;
         return new Promise((resolve, reject) => {
             setImmediate(reject);
@@ -109,9 +109,9 @@ it('should start processing queue items after resume', () => {
 });
 
 it('should not rebase maxConcurrency if a batch of items are pushed at the same time', (done) => {
-    let controllency = new Controllency({ maxConcurrency: 3 });
+    const controllency = new Controllency({ maxConcurrency: 3 });
     let fnCalledCount = 0;
-    let fn = function fn(): Promise<any> {
+    const fn = (): Promise<any> => {
         fnCalledCount += 1;
         return new Promise((resolve, reject) => {
             setImmediate(resolve);
@@ -128,80 +128,84 @@ it('should not rebase maxConcurrency if a batch of items are pushed at the same 
     expect(controllency.getBufferSize()).to.be.eql(6);
     controllency.on('resolved', () => {
         expect(controllency.getCurrentQuantityProcessing()).to.be.lessThan(4);
-        if (fnCalledCount === 6 && controllency.getBufferSize() === 0 && controllency.getCurrentQuantityProcessing() === 0) {
+        if (fnCalledCount === 6 &&
+            controllency.getBufferSize() === 0 &&
+            controllency.getCurrentQuantityProcessing() === 0) {
             expect(controllency.getStatus()).to.be.eql('idle' as ControllencyStatus);
             done();
         }
     });
 });
 
-it('should not rebase maxConcurrency if various batches of items are being pushed while they are being resolved', (done) => {
-    let controllency = new Controllency({ maxConcurrency: 3 });
-    let fnCalledCount = 0;
-    let fnPromisesResolved = 0;
-    let promiseResolver: (() => void)[] = [];
-    let fn = function fn(): Promise<any> {
-        fnCalledCount += 1;
-        return new Promise((resolve, reject) => {
-            promiseResolver.push(resolve);
+it('should not rebase maxConcurrency if various batches of items are being pushed while they are being resolved',
+    (done) => {
+        const controllency = new Controllency({ maxConcurrency: 3 });
+        let fnCalledCount = 0;
+        let fnPromisesResolved = 0;
+        const promiseResolver: Array<() => void> = [];
+        const fn = (): Promise<any> => {
+            fnCalledCount += 1;
+            return new Promise((resolve, reject) => {
+                promiseResolver.push(resolve);
+            });
+        };
+        controllency.push(fn);
+        controllency.push(fn);
+        expect(fnCalledCount).to.be.eql(2);
+        expect(controllency.getCurrentQuantityProcessing()).to.be.eql(2);
+        expect(controllency.getBufferSize()).to.be.eql(2);
+        controllency.push(fn);
+        controllency.push(fn);
+        expect(fnCalledCount).to.be.eql(3);
+        expect(controllency.getCurrentQuantityProcessing()).to.be.eql(3);
+        expect(controllency.getBufferSize()).to.be.eql(4);
+        controllency.push(fn);
+        controllency.push(fn);
+        expect(fnCalledCount).to.be.eql(3);
+        expect(controllency.getCurrentQuantityProcessing()).to.be.eql(3);
+        expect(controllency.getBufferSize()).to.be.eql(6);
+        controllency.on('resolved', () => {
+            expect(controllency.getCurrentQuantityProcessing()).to.be.lessThan(4);
+            expect(fnCalledCount - fnPromisesResolved).to.be.lessThan(4);
+            if (fnCalledCount === 20 &&
+                controllency.getBufferSize() === 0 &&
+                controllency.getCurrentQuantityProcessing() === 0) {
+                expect(controllency.getStatus()).to.be.eql('idle' as ControllencyStatus);
+                done();
+            } else {
+                if (fnPromisesResolved === 4) {
+                    controllency.push(fn);
+                    controllency.push(fn);
+                    controllency.push(fn);
+                    controllency.push(fn);
+                }
+                if (fnPromisesResolved === 8) {
+                    controllency.push(fn);
+                    controllency.push(fn);
+                    controllency.push(fn);
+                    controllency.push(fn);
+                    controllency.push(fn);
+                    controllency.push(fn);
+                    controllency.push(fn);
+                    controllency.push(fn);
+                    controllency.push(fn);
+                }
+                if (fnPromisesResolved === 19) {
+                    controllency.push(fn);
+                }
+                promiseResolver[fnPromisesResolved]();
+                fnPromisesResolved += 1;
+            }
         });
-    };
-    controllency.push(fn);
-    controllency.push(fn);
-    expect(fnCalledCount).to.be.eql(2);
-    expect(controllency.getCurrentQuantityProcessing()).to.be.eql(2);
-    expect(controllency.getBufferSize()).to.be.eql(2);
-    controllency.push(fn);
-    controllency.push(fn);
-    expect(fnCalledCount).to.be.eql(3);
-    expect(controllency.getCurrentQuantityProcessing()).to.be.eql(3);
-    expect(controllency.getBufferSize()).to.be.eql(4);
-    controllency.push(fn);
-    controllency.push(fn);
-    expect(fnCalledCount).to.be.eql(3);
-    expect(controllency.getCurrentQuantityProcessing()).to.be.eql(3);
-    expect(controllency.getBufferSize()).to.be.eql(6);
-    controllency.on('resolved', () => {
-        expect(controllency.getCurrentQuantityProcessing()).to.be.lessThan(4);
-        expect(fnCalledCount - fnPromisesResolved).to.be.lessThan(4);
-        if (fnCalledCount === 20 && controllency.getBufferSize() === 0 && controllency.getCurrentQuantityProcessing() === 0) {
-            expect(controllency.getStatus()).to.be.eql('idle' as ControllencyStatus);
-            done();
-        }
-        else {
-            if (fnPromisesResolved === 4) { 
-                controllency.push(fn);
-                controllency.push(fn);
-                controllency.push(fn);
-                controllency.push(fn);
-            }
-            if (fnPromisesResolved === 8) { 
-                controllency.push(fn);
-                controllency.push(fn);
-                controllency.push(fn);
-                controllency.push(fn);
-                controllency.push(fn);
-                controllency.push(fn);
-                controllency.push(fn);
-                controllency.push(fn);
-                controllency.push(fn);
-            }
-            if (fnPromisesResolved === 19) { 
-                controllency.push(fn);
-            }
-            promiseResolver[fnPromisesResolved]();
-            fnPromisesResolved += 1;
-        }
+        promiseResolver[fnPromisesResolved]();
+        fnPromisesResolved += 1;
     });
-    promiseResolver[fnPromisesResolved]();
-    fnPromisesResolved += 1;
-});
 
 it('should start processing items in the right order', (done) => {
-    let controllency = new Controllency({ maxConcurrency: 3 });
+    const controllency = new Controllency({ maxConcurrency: 3 });
     let nextExpectedItemNumber = 0;
     let invalidOrderOccured = false;
-    let fn = function fn(itemNumber?: number): Promise<any> {
+    const fn = (itemNumber?: number): Promise<any> => {
         if (nextExpectedItemNumber !== itemNumber) {
             invalidOrderOccured = true;
         }
@@ -210,12 +214,13 @@ it('should start processing items in the right order', (done) => {
             setImmediate(resolve);
         });
     };
-    for (let counter = 0; counter < 20; counter += 1){
-        controllency.push({ fn: fn, params: [counter]});
+    for (let counter = 0; counter < 20; counter += 1) {
+        controllency.push({ fn, params: [counter] });
     }
     controllency.on('resolved', () => {
         if (controllency.getBufferSize() === 0 && controllency.getCurrentQuantityProcessing() === 0) {
             expect(controllency.getStatus()).to.be.eql('idle' as ControllencyStatus);
+            // tslint:disable-next-line:no-unused-expression
             expect(invalidOrderOccured).to.be.false;
             done();
         }
@@ -223,11 +228,11 @@ it('should start processing items in the right order', (done) => {
 });
 
 it('should call provided function with the right arguments and "this" value', (done) => {
-    let controllency = new Controllency({ maxConcurrency: 3 });
+    const controllency = new Controllency({ maxConcurrency: 3 });
     let nextExpectedItemIndex = 0;
     let errorOccured = false;
-    let testCases: any[] = [];
-    let fn = function fn(param1: any, param2: any, param3: any): Promise<any> {
+    const testCases: any[] = [];
+    const fn = function(param1: any, param2: any, param3: any): Promise<any> {
         if (this !== testCases[nextExpectedItemIndex].thisObj ||
             param1 !== testCases[nextExpectedItemIndex].params[0] ||
             param2 !== testCases[nextExpectedItemIndex].params[1] ||
@@ -239,64 +244,69 @@ it('should call provided function with the right arguments and "this" value', (d
             setImmediate(resolve);
         });
     };
-    for (let counter = 0; counter < 20; counter += 1){
-        let testCaseItem = { fn: fn, params: [counter, 'a', 'a' + counter], thisObj: { val: counter } };
+    for (let counter = 0; counter < 20; counter += 1) {
+        const testCaseItem = { fn, params: [counter, 'a', 'a' + counter], thisObj: { val: counter } };
         testCases.push(testCaseItem);
         controllency.push(testCaseItem);
     }
     controllency.on('resolved', () => {
         if (controllency.getBufferSize() === 0 && controllency.getCurrentQuantityProcessing() === 0) {
             expect(controllency.getStatus()).to.be.eql('idle' as ControllencyStatus);
+            // tslint:disable-next-line:no-unused-expression
             expect(errorOccured).to.be.false;
             done();
         }
     });
 });
 
-it('should emit "resolved" event with the Promise resolved value as first parameter and the original ControllencyItem as second parameter', (done) => {
-    let controllency = new Controllency({ maxConcurrency: 3 });
-    let errorOccured = false;
-    let fn = function fn(param1: any): Promise<any> {
-        return new Promise((resolve, reject) => {
-            setImmediate(() => { 
-                resolve(param1 * 2);
+it('should emit "resolved" event with the Promise resolved value as first parameter' +
+    ' and the original ControllencyItem as second parameter', (done) => {
+        const controllency = new Controllency({ maxConcurrency: 3 });
+        let errorOccured = false;
+        const fn = (param1: any): Promise<any> => {
+            return new Promise((resolve, reject) => {
+                setImmediate(() => {
+                    resolve(param1 * 2);
+                });
             });
+        };
+        for (let counter = 0; counter < 20; counter += 1) {
+            controllency.push({ fn, params: [counter] });
+        }
+        controllency.on('resolved', (result, controllencyItem) => {
+            if (result !== controllencyItem.params[0] * 2) {
+                errorOccured = true;
+            }
+            if (controllency.getBufferSize() === 0 && controllency.getCurrentQuantityProcessing() === 0) {
+                // tslint:disable-next-line:no-unused-expression
+                expect(errorOccured).to.be.false;
+                done();
+            }
         });
-    };
-    for (let counter = 0; counter < 20; counter += 1){
-        controllency.push({ fn: fn, params: [counter] });
-    }
-    controllency.on('resolved', (result, controllencyItem) => {
-        if (result !== controllencyItem.params[0] * 2) {
-            errorOccured = true;
-        }
-        if (controllency.getBufferSize() === 0 && controllency.getCurrentQuantityProcessing() === 0) {
-            expect(errorOccured).to.be.false;
-            done();
-        }
     });
-});
 
-it('should emit "rejected" event with the Promise rejected reason as first parameter and the original ControllencyItem as second parameter', (done) => {
-    let controllency = new Controllency({ maxConcurrency: 3 });
-    let errorOccured = false;
-    let fn = function fn(param1: any): Promise<any> {
-        return new Promise((resolve, reject) => {
-            setImmediate(() => { 
-                reject(param1 * 2);
+it('should emit "rejected" event with the Promise rejected reason as first parameter' +
+    ' and the original ControllencyItem as second parameter', (done) => {
+        const controllency = new Controllency({ maxConcurrency: 3 });
+        let errorOccured = false;
+        const fn = (param1: any): Promise<any> => {
+            return new Promise((resolve, reject) => {
+                setImmediate(() => {
+                    reject(param1 * 2);
+                });
             });
+        };
+        for (let counter = 0; counter < 20; counter += 1) {
+            controllency.push({ fn, params: [counter] });
+        }
+        controllency.on('rejected', (reason, controllencyItem) => {
+            if (reason !== controllencyItem.params[0] * 2) {
+                errorOccured = true;
+            }
+            if (controllency.getBufferSize() === 0 && controllency.getCurrentQuantityProcessing() === 0) {
+                // tslint:disable-next-line:no-unused-expression
+                expect(errorOccured).to.be.false;
+                done();
+            }
         });
-    };
-    for (let counter = 0; counter < 20; counter += 1){
-        controllency.push({ fn: fn, params: [counter] });
-    }
-    controllency.on('rejected', (reason, controllencyItem) => {
-        if (reason !== controllencyItem.params[0] * 2) {
-            errorOccured = true;
-        }
-        if (controllency.getBufferSize() === 0 && controllency.getCurrentQuantityProcessing() === 0) {
-            expect(errorOccured).to.be.false;
-            done();
-        }
     });
-});
